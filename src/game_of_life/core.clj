580f1189d97a0dd4dc0175afa89467be0d5dfc97
hindenbug <manoj.mk27@gmail.com>
2015-07-31 02:14:05 +0530

    change approach

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -10,18 +10,39 @@
 (def east [1 0])
 (def west [-1 0])
 
-(defn setup-world [w h & alive]
-  (vec (for [y (range w)]
-         (vec (for [x (range h)]
-                (if (contains? (first alive) [y x])
-                  "x"
-                  " "))))))
-
-(defn alive)
+(defn setup-w [w h]
+  (vec (repeat w (vec (repeat h nil)))))
 
 (defn neighbours [[x y]]
-  (set (for [i (range -1 2)
+  (vec (for [i (range -1 2)
              j (range -1 2)
-             :when (not (and (= x i) (= y j)))]
-         [i j])))
+             :when (not= 0 i j)]
+         [(+ x i) (+ y j)])))
+
+(defn all-alive-cells [world]
+  (vec (for [[x row] (map-indexed vector world)
+             [y value] (map-indexed vector row)]
+         [x y])))
+
+(defn alive-neighbours [world cell]
+  (count (filter #(= "x" (get-in world %))
+                 (neighbours cell))))
+
+(defn survive? [world cell]
+  (let [alive-nbrs (alive-neighbours world cell)]
+    (and (nil? (get-in world cell)) (= 3 alive-nbrs))))
+      ;;(or (= alive-nbrs 2) (= alive-nbrs 3))))
+
+(defn tick [world]
+  (reduce (fn [world loc]
+            (if (survive? world loc)
+              (assoc-in world loc "x")
+              (assoc-in world loc nil)))
+          world (all-alive-cells world)))
+
+;; alive as set of cordinates
+(defn setup-world [w h cells]
+  (reduce (fn [world loc]
+            (assoc-in world loc "x"))
+          (setup-w w h) cells))
 
